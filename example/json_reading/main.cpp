8f51eda44b363aee2b8c970102f2ff065885a0f0
dvevlanov <denis.evlanov@gmail.com>
2024-04-21 01:18:03 +0300

    add test benchmark for reading json

--- example/json_reading/main.cpp
+++ example/json_reading/main.cpp
@@ -7,5 +7,5 @@ int main(void) {
     ondemand::parser parser;
     padded_string json = padded_string::load("twitter.json");
     ondemand::document tweets = parser.iterate(json);
-    std::cout << uint64_t(tweets["search_metadata"]["count"]) << " results." << std::endl;
+    std::cout << std::string_view(tweets["k"]["o"]);
 }
--- test/benchmark/json_reader/jsonreader.cpp
+++ test/benchmark/json_reader/jsonreader.cpp
@@ -1,18 +1,26 @@
 #include <benchmark/benchmark.h>
+#include <iostream>
+#include "simdjson.h"
+
+#define FMT_HEADER_ONLY
+#include <bybit/third_party/fmt/core.h>
+using namespace simdjson;
+
+std::string current_path = PATH_TO_TEST;
 
 static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
+    ondemand::parser parser;
+    padded_string json = padded_string::load(fmt::format("{0}/{1}", current_path, "input.txt"));
+
+    for (auto _ : state)
+    {
+        ondemand::document tweets = parser.iterate(json);
+        auto v = std::string_view(tweets["k"]["o"]);
+        //std::cout << std::string_view(tweets["k"]["o"]);//<< (tweets["k"]["c"]);
+    }
 }
 // Register the function as a benchmark
 BENCHMARK(BM_StringCreation);
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
 
 BENCHMARK_MAIN();
