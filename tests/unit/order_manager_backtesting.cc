43dd2a63104e16fb4aa7843e05b070b64dacf8be
Denis Evlanov <deniska-evlanov@mail.ru>
2024-09-05 23:29:08 +0300

    check cancel order

--- tests/unit/order_manager_backtesting.cc
+++ tests/unit/order_manager_backtesting.cc
@@ -1,39 +1,57 @@
-#include "cmath"
-
-#include "gtest/gtest.h"
-
+#include "aot/Binance.h"
 #include "aot/Logger.h"
 #include "aot/strategy/market_order_book.h"
 #include "aot/strategy/trade_engine.h"
-#include "aot/Binance.h"
+#include "cmath"
+#include "gtest/gtest.h"
 
-TEST(order_manager_backtesting, InsertFirstOrder) {
-    TickerHashMap tickers;
-    tickers[1] = "usdt";
-    tickers[2] = "btc";
-    
-    TradingPairHashMap pair;
-    binance::Symbol symbol(tickers[2], tickers[1]);
-    TradingPairInfo pair_info{std::string(symbol.ToString()), 2, 5};
-    pair[{2, 1}] = pair_info;
+class OrdermanagerBacktestingTest : public testing::Test {
+  public:
+    TickerHashMap tickers_;
+    TradingPairHashMap pair_;
+    Exchange::EventLFQueue market_updates_;
+    OHLCVILFQueue klines_;
+    Common::TradingPair trading_pair_;
+    Common::TradingPairHashMap pairs_;
+    backtesting::TradeEngine trade_engine_;
+    backtesting::OrderManager order_manager_;
 
-    Exchange::EventLFQueue market_updates;
-    OHLCVILFQueue klines;
-    Common::TradingPair trading_pair;
-    Common::TradingPairHashMap pairs;
-    backtesting::TradeEngine trade_engine(&market_updates, &klines, Common::TradingPair{2,1}, pair, nullptr);
+  protected:
+    OrdermanagerBacktestingTest()
+        : trade_engine_(&market_updates_, &klines_, Common::TradingPair{2, 1},
+                        pair_, nullptr),
+          order_manager_(&trade_engine_) {
+        tickers_[1] = "usdt";
+        tickers_[2] = "btc";
+        binance::Symbol symbol(tickers_[2], tickers_[1]);
+        TradingPairInfo pair_info{std::string(symbol.ToString()), 2, 5};
+        pair_[{2, 1}] = pair_info;
+        order_manager_.NewOrder(Common::TradingPair{2, 1}, 33.1,
+                                Common::Side::BUY, 7.2);
+    };
+};
 
-    backtesting::OrderManager order_manager(&trade_engine);
-    order_manager.NewOrder(Common::TradingPair{2,1}, 33.1, Common::Side::BUY, 7.2);
-    auto order = order_manager.TestGetOrder(Common::TradingPair{2,1}, Common::Side::BUY);
-    EXPECT_EQ(order->price,33.1);
-    EXPECT_EQ(order->order_id,1);
-    EXPECT_EQ(order->qty,7.2);
-    EXPECT_EQ(order->side,Common::Side::BUY);
-    EXPECT_EQ(order->trading_pair,(TradingPair{2,1}));
+TEST_F(OrdermanagerBacktestingTest, InsertFirstOrder) {
+    auto order = order_manager_.TestGetOrder(Common::TradingPair{2, 1},
+                                             Common::Side::BUY);
+    EXPECT_EQ(order->price, 33.1);
+    EXPECT_EQ(order->order_id, 1);
+    EXPECT_EQ(order->qty, 7.2);
+    EXPECT_EQ(order->side, Common::Side::BUY);
+    EXPECT_EQ(order->trading_pair, (TradingPair{2, 1}));
+}
+
+TEST_F(OrdermanagerBacktestingTest, CancelOrder) {
+    order_manager_.CancelOrder(Common::TradingPair{2, 1},
+                                             Common::Side::BUY);
+    auto order = order_manager_.TestGetOrder(Common::TradingPair{2, 1},
+                                             Common::Side::BUY);
+    EXPECT_EQ(order->order_id, 1);
+    EXPECT_EQ(order->side, Common::Side::BUY);
+    EXPECT_EQ(order->trading_pair, (TradingPair{2, 1}));
 }
 
 int main(int argc, char** argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
