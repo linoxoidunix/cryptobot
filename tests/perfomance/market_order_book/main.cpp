a9e9a294c3ff85c6f1054a697aa097e12316a6a6
Denis Evlanov <deniska-evlanov@mail.ru>
2024-09-12 00:57:29 +0300

    fix errors in test

--- tests/perfomance/market_order_book/main.cpp
+++ tests/perfomance/market_order_book/main.cpp
@@ -24,51 +24,50 @@ class FixtureMarketOrderBook : public benchmark::Fixture {
             "qty:(\\d+\\.\\d+) price:(\\d+\\.\\d+)\\]");
         std::smatch pieces_match;
         diffs.reserve(200000);
-        diffs_double.reserve(200000);
         while (std::getline(infile, line)) {
             if (std::regex_match(line, pieces_match, word_regex)) {
                 Exchange::MEMarketUpdate update;
-                Exchange::MEMarketUpdateDouble update_double;
 
                 auto _type = magic_enum::enum_cast<Exchange::MarketUpdateType>(
                     std::string(pieces_match[2]));
                 if (!_type.has_value()) continue;
                 update.type        = _type.value();
-                update_double.type = _type.value();
                 auto _side         = magic_enum::enum_cast<common::Side>(
                     std::string(pieces_match[3]));
                 if (!_side.has_value()) continue;
                 update.side         = _side.value();
-                update_double.side  = _side.value();
 
                 double qty          = stod(pieces_match[4]);
-                update_double.qty   = qty;
                 double price        = stod(pieces_match[5]);
-                update_double.price = price;
 
                 update.qty          = qty * 10000;
                 update.price        = price * 100;
                 diffs.push_back(update);
-                diffs_double.push_back(update_double);
             }
         }
     }
 
     void TearDown(::benchmark::State& state) override{
         diffs.clear();
-        diffs_double.clear();
     }
 
     std::vector<Exchange::MEMarketUpdate> diffs;
-    std::vector<Exchange::MEMarketUpdateDouble> diffs_double;
 };
 
 BENCHMARK_DEFINE_F(FixtureMarketOrderBook, TestMarketOrderBook)(benchmark::State& st) {
-    Trading::MarketOrderBook book;
+    common::TickerHashMap tickers;
+    tickers[1] = "usdt";
+    tickers[2] = "btc";
+    
+    common::TradingPairHashMap pair;
+    binance::Symbol symbol(tickers[2], tickers[1]);
+    common::TradingPairInfo pair_info{std::string(symbol.ToString()), 2, 5};
+    pair[{2, 1}] = pair_info;
+    backtesting::MarketOrderBook book(common::TradingPair{2,1}, pair);
     for (auto _ : st) {
         auto begin = common::getCurrentNanoS();
         for (int i = 0; i < diffs.size(); i++) {
-            book.onMarketUpdate(&diffs[i]);
+            book.OnMarketUpdate(&diffs[i]);
         }
         auto end             = common::getCurrentNanoS();
         auto elapsed_seconds = (end - begin) / diffs.size() * 1e-9;
