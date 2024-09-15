c170dd1e97243e7e4ed2e1816401fe34eff91559
Denis Evlanov <deniska-evlanov@mail.ru>
2024-09-15 11:16:24 +0300

    start impl cross arbitrage strategy/ it was compiled

--- aot/strategy/base_strategy.h
+++ aot/strategy/base_strategy.h
@@ -1,8 +1,11 @@
 #pragma once
+#include <list>
+#include <unordered_map>
 
 #include "aot/Exchange.h"
 #include "aot/Logger.h"
 #include "aot/common/macros.h"
+#include "aot/strategy/market_order.h"
 #include "aot/strategy/market_order_book.h"
 #include "aot/strategy/order_manager.h"
 #include "aot/wallet_asset.h"
@@ -20,15 +23,15 @@ class BaseStrategy {
                           OrderManager *order_manager,
                           const TradeEngineCfgHashMap &ticker_cfg,
                           const common::TradingPair trading_pairs,
-                          common::TradingPairHashMap& pairs);
+                          common::TradingPairHashMap &pairs);
 
     /**
      * Launch OnOrderBookUpdate callback when there are changes in
      * marketorderbookdouble for BaseStrategy is None
      */
-    auto OnOrderBookUpdate(const common::TradingPair &trading_pair, common::Price price, Side side,
-                           Trading::MarketOrderBook *order_book) noexcept
-        -> void {
+    auto OnOrderBookUpdate(
+        const common::TradingPair &trading_pair, common::Price price, Side side,
+        Trading::MarketOrderBook *order_book) noexcept -> void {
         if (!order_book_) [[unlikely]]
             order_book_ = order_book;
     }
@@ -65,10 +68,11 @@ class BaseStrategy {
     Trading::OrderManager *order_manager_ = nullptr;
     const TradeEngineCfgHashMap &ticker_cfg_;
     TradingPair trading_pairs_;
-    TradingPairHashMap& pairs_;
+    TradingPairHashMap &pairs_;
     Wallet wallet_;
     Trading::MarketOrderBook *order_book_ = nullptr;
-    std::vector<std::function<void(const common::TradingPair& trading_pair)>> actions_;
+    std::vector<std::function<void(const common::TradingPair &trading_pair)>>
+        actions_;
     /**
      * @brief if strategy want buy qty asset with price_asset=price it calls
      * this BuySomeAsset. Used for long operation
@@ -77,7 +81,7 @@ class BaseStrategy {
      * @param price
      * @param qty
      */
-    auto BuySomeAsset(const common::TradingPair& pair) noexcept -> void {
+    auto BuySomeAsset(const common::TradingPair &pair) noexcept -> void {
         if (!order_book_) [[unlikely]] {
             logi("order_book_ ptr not updated in strategy");
             return;
@@ -87,9 +91,10 @@ class BaseStrategy {
             return;
         }
         auto buy_price = order_book_->getBBO()->ask_price;
-        if(buy_price == common::kPriceInvalid)[[unlikely]]
-        {
-            logw("skip BuySomeAsset. BBO ask_price=INVALID. please wait more time for update BBO");
+        if (buy_price == common::kPriceInvalid) [[unlikely]] {
+            logw(
+                "skip BuySomeAsset. BBO ask_price=INVALID. please wait more "
+                "time for update BBO");
             return;
         }
         auto qty = ticker_cfg_.at(pair).clip;
@@ -104,7 +109,8 @@ class BaseStrategy {
      * @param trading_pair
      * @param price
      */
-    auto SellAllAsset(const common::TradingPair& trading_pair) noexcept -> void {
+    auto SellAllAsset(const common::TradingPair &trading_pair) noexcept
+        -> void {
         logi("launch long sell action for {}", trading_pair.ToString());
         if (!order_book_) [[unlikely]] {
             logi("order_book_ ptr not updated in strategy");
@@ -113,7 +119,8 @@ class BaseStrategy {
         if (auto number_asset = wallet_.SafetyGetNumberAsset(trading_pair);
             number_asset > 0) {
             auto price = order_book_->getBBO()->bid_price;
-            order_manager_->NewOrder(trading_pair, price, Side::SELL, std::abs(number_asset));
+            order_manager_->NewOrder(trading_pair, price, Side::SELL,
+                                     std::abs(number_asset));
         } else
             logw("fail because number_asset={} <= 0", number_asset);
     };
@@ -123,33 +130,37 @@ class BaseStrategy {
      *
      * @param trading_pair
      */
-    auto SellSomeAsset(const common::TradingPair& trading_pair) noexcept -> void {
+    auto SellSomeAsset(const common::TradingPair &trading_pair) noexcept
+        -> void {
         if (!order_book_) [[unlikely]] {
             logi("order_book_ ptr not updated in strategy");
             return;
         }
         if (!ticker_cfg_.count(trading_pair)) [[unlikely]] {
-            logw("fail sell because tickercfg not contain {}", trading_pair.ToString());
+            logw("fail sell because tickercfg not contain {}",
+                 trading_pair.ToString());
             return;
         }
         auto sell_price = order_book_->getBBO()->bid_price;
-        if(sell_price == common::kPriceInvalid)
-        {
-            logw("skip SellSomeAsset. BBO bid_price=INVALID. please wait more time for update BBO");
+        if (sell_price == common::kPriceInvalid) {
+            logw(
+                "skip SellSomeAsset. BBO bid_price=INVALID. please wait more "
+                "time for update BBO");
             return;
         }
         auto qty = ticker_cfg_.at(trading_pair).clip;
         logi("launch short sell action for {} price:{} qty:{}",
              trading_pair.ToString(), sell_price, qty);
-        order_manager_->NewOrder(trading_pair, sell_price, Side::SELL, std::abs(qty));
+        order_manager_->NewOrder(trading_pair, sell_price, Side::SELL,
+                                 std::abs(qty));
     };
     /**
      * @brief if strategy want buy all asset that early sold than it calls
      * BuyAllAsset. Used for short operation
      *
-     * @param trading_pair 
+     * @param trading_pair
      */
-    auto BuyAllAsset(const common::TradingPair& trading_pair) noexcept -> void {
+    auto BuyAllAsset(const common::TradingPair &trading_pair) noexcept -> void {
         logi("launch short buy action for {}", trading_pair.ToString());
         if (!order_book_) [[unlikely]] {
             logi("order_book_ ptr not updated in strategy");
@@ -169,36 +180,93 @@ class BaseStrategy {
      */
     void InitActions() {
         actions_.resize((int)TradeAction::kNope + 1);
-        actions_[(int)TradeAction::kEnterLong] = [this](const common::TradingPair& trading_pair) {
-            BuySomeAsset(trading_pair);
-        };
-        actions_[(int)TradeAction::kEnterShort] = [this](const common::TradingPair& trading_pair) {
-            SellSomeAsset(trading_pair);
-        };
-        actions_[(int)TradeAction::kExitLong] = [this](const common::TradingPair& trading_pair) {
-            SellAllAsset(trading_pair);
-        };
-        actions_[(int)TradeAction::kExitShort] = [this](const common::TradingPair& trading_pair) {
-            BuyAllAsset(trading_pair);
-        };
-        actions_[(int)TradeAction::kNope] = [this](const common::TradingPair& trading_pair) {
-        };
+        actions_[(int)TradeAction::kEnterLong] =
+            [this](const common::TradingPair &trading_pair) {
+                BuySomeAsset(trading_pair);
+            };
+        actions_[(int)TradeAction::kEnterShort] =
+            [this](const common::TradingPair &trading_pair) {
+                SellSomeAsset(trading_pair);
+            };
+        actions_[(int)TradeAction::kExitLong] =
+            [this](const common::TradingPair &trading_pair) {
+                SellAllAsset(trading_pair);
+            };
+        actions_[(int)TradeAction::kExitShort] =
+            [this](const common::TradingPair &trading_pair) {
+                BuyAllAsset(trading_pair);
+            };
+        actions_[(int)TradeAction::kNope] =
+            [this](const common::TradingPair &trading_pair) {};
     };
 };
 };  // namespace Trading
 
-namespace strategy{
-    namespace cross_arbitrage{
-        class CrossArbitrageStrategy : public Trading::BaseStrategy {
-            public:
-            explicit CrossArbitrageStrategy(base_strategy::Strategy *strategy,
-                                         Trading::TradeEngine *trade_engine,
-                                         Trading::OrderManager *order_manager,
-                                         const TradeEngineCfgHashMap &ticker_cfg,
-                                         const common::TradingPair trading_pairs,
-                                         common::TradingPairHashMap& pairs):
-                                         Trading::BaseStrategy(strategy, trade_engine, order_manager, ticker_cfg, trading_pairs, pairs){};
-            // void OnNewSignal(Event* event)
-        };
+namespace strategy {
+namespace cross_arbitrage {
+
+class CrossArbitrageStrategy : public Trading::BaseStrategy {
+    std::unordered_map<common::ExchangeId, Trading::BBO> prices_;
+    std::unordered_map<common::ExchangeId, common::TradingPair> &working_pairs_;
+    std::list<common::ExchangeId> &exchanges_;
+    common::ExchangeId exch1;
+    common::ExchangeId exch2;
+
+  public:
+    explicit CrossArbitrageStrategy(
+        std::unordered_map<common::ExchangeId, common::TradingPair>
+            &working_pairs,
+        std::list<common::ExchangeId> &exchanges,
+        Trading::TradeEngine *trade_engine,
+        Trading::OrderManager *order_manager,
+        const TradeEngineCfgHashMap &ticker_cfg,
+        const common::TradingPair trading_pairs,
+        common::TradingPairHashMap &pairs)
+        : Trading::BaseStrategy(nullptr, trade_engine, order_manager,
+                                ticker_cfg, trading_pairs, pairs), working_pairs_(working_pairs), exchanges_(exchanges) {
+        exch1 = *exchanges_.begin();
+        exch2 = *exchanges_.rbegin();
+    };
+    void OnNewSignal(Event *signal) {
+        auto type = signal->GetType();
+        if (type == strategy::cross_arbitrage::EventType::kBidUpdate) {
+            {
+                auto event =
+                    static_cast<strategy::cross_arbitrage::BBidUpdated *>(
+                        signal);
+                prices_[event->exchange].bid_price = event->price;
+                prices_[event->exchange].bid_qty  = event->qty;
+                event->Deallocate();
+            }
+        } else if (type == strategy::cross_arbitrage::EventType::kAskUpdate) {
+            auto event =
+                static_cast<strategy::cross_arbitrage::BAskUpdated *>(signal);
+            prices_[event->exchange].ask_price = event->price;
+            prices_[event->exchange].ask_qty  = event->qty;
+            event->Deallocate();
+        } else {
+            loge("Unknown signal type");
+            return;
+        }
+        bool condition1 = prices_[exch2].bid_price == common::kPriceInvalid ||
+                          prices_[exch1].ask_price == common::kPriceInvalid;
+        if (condition1)
+            if (prices_[exch2].bid_price - prices_[exch1].ask_price > 0) {
+                logi("buy on exch1 and sell on exch2");
+                return;
+            }
+        bool condition2 = prices_[exch1].bid_price == common::kPriceInvalid ||
+                          prices_[exch2].ask_price == common::kPriceInvalid;
+
+        if (condition2)
+            if (prices_[exch1].bid_price - prices_[exch2].ask_price > 0) {
+                logi("buy on exch2 and sell on exch1");
+                return;
+            }
+        if (!condition1 && !condition2) {
+            logw("there aren't conditions for cross arbitrage");
+        }
     };
 };
+};  // namespace cross_arbitrage
+};  // namespace strategy
